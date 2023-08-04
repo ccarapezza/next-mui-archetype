62b868fc4d88e943522a1a62a5cecb3bfa451a71
Jorge federico Montillo <jf.montillo@gmail.com>
2023-08-03 21:39:22 -0300

    Uncommented carousel

--- src/components/store/carousels/CarouselProducts.tsx
+++ src/components/store/carousels/CarouselProducts.tsx
@@ -94,7 +94,7 @@ export default function (props: { products: any[] }) {
   const firstPageSlider = props.products.slice(0, 4);
   const secondPageSlider = props.products.slice(4, 8);
   const paginationSlider = [firstPageSlider];
-  if(props.products.length > 4) {
+  if (props.products.length > 4) {
     paginationSlider.push(secondPageSlider);
   }
 
@@ -116,51 +116,47 @@ export default function (props: { products: any[] }) {
   return (
     <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
       <h3 className='text-5xl font-tungsten text-primary sm:text-6xl mb-4 text-center'>NUESTRA SELECCIÓN</h3>
-        {JSON.stringify(props.products)}
-        {/*
+      {/* {JSON.stringify(props.products)} */}
+      <Carousel
+        navButtonsAlwaysVisible={true} // Show next and previous buttons
+        animation='slide'               // Defines the animation style of the Carousel
+        duration={1400}                  // Define the duration of the transition in ms. Default is 500 ms
+        fullHeightHover={false}        // We want the nav buttons wrapper to only be as big as the button element is
+        navButtonsProps={{             // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
+          style: {
+            color: '#EFE6D9',
+            backgroundColor: 'transparent',
+            borderRadius: 50
+          }
+        }}
+        indicatorContainerProps={{
+          style: {
+            marginTop: '30px'
+          }
 
-            <Carousel
-                navButtonsAlwaysVisible={true} // Show next and previous buttons
-                animation='slide'               // Defines the animation style of the Carousel
-                duration={1400}                  // Define the duration of the transition in ms. Default is 500 ms
-                fullHeightHover={false}        // We want the nav buttons wrapper to only be as big as the button element is
-                navButtonsProps={{             // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
-                style: {
-                    color: '#EFE6D9',
-                    backgroundColor: 'transparent',
-                    borderRadius: 50
-                }
-                }}
-                indicatorContainerProps={{
-                style: {
-                    marginTop: '30px'
-                }
-        
-            }}
-                NextIcon={<FontAwesomeIcon icon={faChevronRight} />}
-                PrevIcon={<FontAwesomeIcon icon={faChevronLeft} />}
-            >
-                {
-                windowWidth > 768 ?
-                    paginationSlider.map((arg, i) => {
-                    return (
-                        <div key={i} className='grid gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-4 mx-1'>
-                        {
-                            arg.map((product, i) => <ProductCard key={i} product={product} />)
-                        }
-                        </div>
-                    )
-                    })
-                    :
-                    props.products.map((product, i) => {
-                    return (
-                        <ProductCard key={i} product={product} />
-                    )
-                    })
-                }
-            </Carousel>
-        */}
-
+        }}
+        NextIcon={<FontAwesomeIcon icon={faChevronRight} />}
+        PrevIcon={<FontAwesomeIcon icon={faChevronLeft} />}
+      >
+        {
+          windowWidth > 768 ?
+            paginationSlider.map((arg, i) => {
+              return (
+                <div key={i} className='grid gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-4 mx-1'>
+                  {
+                    arg.map((product, i) => <ProductCard key={i} product={product} />)
+                  }
+                </div>
+              )
+            })
+            :
+            props.products.map((product, i) => {
+              return (
+                <ProductCard key={i} product={product} />
+              )
+            })
+        }
+      </Carousel> 
     </div>
   )
 }
