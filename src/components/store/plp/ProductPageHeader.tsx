786d0bb817e8c3e6e1fc00c21314fcfedcd74568
Jorge federico Montillo <jf.montillo@gmail.com>
2023-08-04 12:52:10 -0300

    Add category name in plp page

--- src/components/store/plp/ProductPageHeader.tsx
+++ src/components/store/plp/ProductPageHeader.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function () {
+export default function (props: { categoryTitle: string }) {
 
   return (
     <header className="text-center">
-      <h2 className="font-tungsten text-primary text-6xl mb-4 text-center">
-        Hombre
+      <h2 className="font-tungsten text-primary text-6xl mb-4 text-center capitalize">
+        {props.categoryTitle}
       </h2>
 
       <p className="max-w-md mx-auto mt-4 text-tertiary">
