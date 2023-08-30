9d706096f8e01aa4b4ee4ab76495a6d122906cad
ccarapezza <carapezza.christian@gmail.com>
2023-08-29 21:15:23 -0300

    ProductCategoryService fix

--- src/app/api/management/category/list/route.ts
+++ src/app/api/management/category/list/route.ts
@@ -1,11 +1,9 @@
-import { ProductCategory } from '@/db';
-import { ProductCategoryService } from '@/services/ProductCategoryService';
+import { productCategoryService } from '@/services/ProductCategoryService';
 import { NextRequest, NextResponse } from 'next/server'
-import { Op, WhereOptions } from 'sequelize';
 
 export async function GET(request: NextRequest) {
     console.log("search", request.nextUrl.searchParams.get("search"))
     const searchTerm = request.nextUrl.searchParams.get("search");
-    const productCategory = ProductCategoryService.searchCategoryTree(searchTerm);
+    const productCategory = productCategoryService.searchCategoryTree(searchTerm);
     return NextResponse.json(productCategory);
 }
