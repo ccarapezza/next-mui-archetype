4421f53b01742d551d8007da406ece761f15c557
ccarapezza <carapezza.christian@gmail.com>
2023-08-30 03:37:24 -0300

    ✔ No ESLint warnings or errors

--- src/components/store/minicart/MiniCart.tsx
+++ src/components/store/minicart/MiniCart.tsx
@@ -8,85 +8,80 @@ import MiniCartTotalizer from './MiniCartTotalizer';
 
 export default function MiniCart() {
 
-  const [openMiniCart, setOpenMiniCart] = useState(false)
-  const { products } = useContext(CartContext)
-  const [quantity, setQuantity] = useState(null as number | null)
-  useEffect(() => {
-    if (quantity === null) {
-      setOpenMiniCart(false);
-      setQuantity(products.length);
-    } else {
-      setQuantity(products.length);
-      setOpenMiniCart(true);
-    }
-  }, [products, quantity])
-
-  return (
-    <>
-      {/* Trigger Minicart */}
-      <div className="text-xl cursor-pointer h-8 flex items-center justify-center w-8 relative"
-        onClick={() => {
-          setOpenMiniCart(true)
-        }}>
-        {
-          quantity != null && quantity > 0 ?
-            <span
-              className='text-tertiary bg-white rounded-full'
-              style={{
-                padding: "0px 4px",
-                fontSize: "15px",
-                fontWeight: "600",
-                border: "2px solid #322F30",
-                lineHeight: "1",
-                position: "absolute",
-                bottom: "18px",
-                left: "17px",
-              }}>
-              {quantity}
-            </span>
-            :
-            <span>
-            </span>
+    const [openMiniCart, setOpenMiniCart] = useState(false)
+    const { products } = useContext(CartContext)
+    useEffect(() => {
+        if (!openMiniCart && products.length > 0) {
+            setOpenMiniCart(true);
         }
-        <FontAwesomeIcon className='text-tertiary hover:text-primary' icon={faCartShopping} />
-      </div>
-      {/* Modal Minicart */}
-      {
-        openMiniCart ?
-          (
-            <div className="fixed inset-0 z-10 overflow-y-auto">
-              <div className="fixed inset-0 w-full h-full bg-black opacity-40" onClick={() => setOpenMiniCart(false)}></div>
-              <div className="flex items-left">
-                <div className="flex flex-col justify-between w-full max-w-lg mx-auto bg-white shadow-lg h-screen absolute right-0">
-                  <div>
-                    <div className="flex items-center justify-between p-4 border-b">
-                      <h4 className="text-lg text-stone-500 font-bold">
-                        Tú Carrito:
-                      </h4>
-                      <button className="p-2 text-gray-400 rounded-md hover:bg-gray-100"
-                        onClick={() => setOpenMiniCart(false)}
-                      >
-                        <FontAwesomeIcon icon={faClose} className='w-6 h-6' />
-                      </button>
-                    </div>
-                    {
-                      quantity != null && quantity > 0 ?
-                        <div className="space-y-2 p-4 mt-3 text-[15.5px] leading-relaxed text-gray-500">
-                          <MiniCartList products={products} />
-                        </div>
+    }, [products.length, openMiniCart])
+
+    return (
+        <>
+            {/* Trigger Minicart */}
+            <div className="text-xl cursor-pointer h-8 flex items-center justify-center w-8 relative"
+                onClick={() => {
+                    setOpenMiniCart(true)
+                }}>
+                {
+                    products.length != null && products.length > 0 ?
+                        <span
+                            className='text-tertiary bg-white rounded-full'
+                            style={{
+                                padding: "0px 4px",
+                                fontSize: "15px",
+                                fontWeight: "600",
+                                border: "2px solid #322F30",
+                                lineHeight: "1",
+                                position: "absolute",
+                                bottom: "18px",
+                                left: "17px",
+                            }}>
+                            {products.length}
+                        </span>
                         :
-                        <div className="flex items-center justify-center h-full">
-                          <p className='text-stone-500 font-bold text-lg'>¡Tú carrito está vacío!</p>
-                        </div>
-                    }
-                  </div>
-                  <MiniCartTotalizer quantity={quantity} />
-                </div>
-              </div>
+                        <span>
+                        </span>
+                }
+                <FontAwesomeIcon className='text-tertiary hover:text-primary' icon={faCartShopping} />
             </div>
-          )
-          : ''
-      }
-    </>
-  )
+            {/* Modal Minicart */}
+            {
+                openMiniCart ?
+                    (
+                        <div className="fixed inset-0 z-10 overflow-y-auto">
+                            <div className="fixed inset-0 w-full h-full bg-black opacity-40" onClick={() => setOpenMiniCart(false)}></div>
+                            <div className="flex items-left">
+                                <div className="flex flex-col justify-between w-full max-w-lg mx-auto bg-white shadow-lg h-screen absolute right-0">
+                                    <div>
+                                        <div className="flex items-center justify-between p-4 border-b">
+                                            <h4 className="text-lg text-stone-500 font-bold">
+                                                Tú Carrito:
+                                            </h4>
+                                            <button className="p-2 text-gray-400 rounded-md hover:bg-gray-100"
+                                                onClick={() => setOpenMiniCart(false)}
+                                            >
+                                                <FontAwesomeIcon icon={faClose} className='w-6 h-6' />
+                                            </button>
+                                        </div>
+                                        {
+                                            products.length != null && products.length > 0 ?
+                                                <div className="space-y-2 p-4 mt-3 text-[15.5px] leading-relaxed text-gray-500">
+                                                    <MiniCartList products={products} />
+                                                </div>
+                                                :
+                                                <div className="flex items-center justify-center h-full">
+                                                    <p className='text-stone-500 font-bold text-lg'>¡Tú carrito está vacío!</p>
+                                                </div>
+                                        }
+                                    </div>
+                                    <MiniCartTotalizer quantity={products.length} />
+                                </div>
+                            </div>
+                        </div>
+                    )
+                    : ''
+            }
+        </>
+    )
 }
--- src/components/store/plp/filters/PricePicker.tsx
+++ src/components/store/plp/filters/PricePicker.tsx
@@ -4,109 +4,108 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
-export default function PricePicker(props: { filters: any, setFilters: any }) {
+export default function PricePicker({ filters, setFilters }: { filters: any, setFilters: any }) {
+    const refFrom = useRef<HTMLInputElement>(null);
+    const refTo = useRef<HTMLInputElement>(null);
 
-  const { filters, setFilters } = props;
-  const refFrom = useRef<HTMLInputElement>(null);
-  const refTo = useRef<HTMLInputElement>(null);
+    const [priceFrom, setPriceFrom] = useState<string>(filters?.selectedPrice?.from ? filters?.selectedPrice?.from : '');
+    const [priceTo, setPriceTo] = useState<string>(filters?.selectedPrice?.to ? filters?.selectedPrice?.to : '');
+    const debouncedPriceFrom = useDebounce(priceFrom, 1000);
+    const debouncedPriceTo = useDebounce(priceTo, 1000);
 
-  const [priceFrom, setPriceFrom] = useState<string>(filters?.selectedPrice?.from ? filters?.selectedPrice?.from : '');
-  const [priceTo, setPriceTo] = useState<string>(filters?.selectedPrice?.to ? filters?.selectedPrice?.to : '');
-  const debouncedPriceFrom = useDebounce(priceFrom, 1000);
-  const debouncedPriceTo = useDebounce(priceTo, 1000);
+    const handleChangeFrom = (e: any) => {
+        setPriceFrom(e.target.value);
+    };
 
-  const handleChangeFrom = (e: any) => {
-    setPriceFrom(e.target.value);
-  };
+    const handleChangeTo = (e: any) => {
+        setPriceTo(e.target.value);
+    };
 
-  const handleChangeTo = (e: any) => {
-    setPriceTo(e.target.value);
-  };
+    useEffect(() => {
+        setFilters({
+            ...filters,
+            selectedPrice:
+                { from: debouncedPriceFrom, to: debouncedPriceTo, }
+        })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [debouncedPriceFrom, debouncedPriceTo])
 
-  useEffect(() => {
-    setFilters({
-      ...filters,
-      selectedPrice:
-        { from: debouncedPriceFrom, to: debouncedPriceTo, }
-    })
-  }, [debouncedPriceFrom, debouncedPriceTo, filters, setFilters])
-
-  useEffect(() => {
-    if (refFrom.current != null) {
-      refFrom.current.value = filters.selectedPrice.from.toString();
-    }
-    if (refTo.current != null) {
-      refTo.current.value = filters.selectedPrice.to.toString();
-    }
-  }, [filters.selectedPrice])
+    useEffect(() => {
+        if (refFrom.current != null) {
+            refFrom.current.value = filters.selectedPrice.from.toString();
+        }
+        if (refTo.current != null) {
+            refTo.current.value = filters.selectedPrice.to.toString();
+        }
+    }, [filters.selectedPrice])
 
 
-  return (
-    <details
-      className="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden"
-      open={true}
-    >
-      <summary
-        className="flex cursor-pointer items-center justify-between gap-2 bg-white p-4 text-gray-900 transition"
-      >
-        <span className="text-sm font-medium"> Precio </span>
-        <span className="transition group-open:-rotate-180">
-          <FontAwesomeIcon icon={faChevronDown} className="h-4 w-4" />
-        </span>
-      </summary>
+    return (
+        <details
+            className="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden"
+            open={true}
+        >
+            <summary
+                className="flex cursor-pointer items-center justify-between gap-2 bg-white p-4 text-gray-900 transition"
+            >
+                <span className="text-sm font-medium"> Precio </span>
+                <span className="transition group-open:-rotate-180">
+                    <FontAwesomeIcon icon={faChevronDown} className="h-4 w-4" />
+                </span>
+            </summary>
 
-      <div className="border-t border-gray-200 bg-white">
-        <header className="flex items-center justify-between p-4">
-          <span className="text-sm text-gray-700"> Seleccione el rango </span>
+            <div className="border-t border-gray-200 bg-white">
+                <header className="flex items-center justify-between p-4">
+                    <span className="text-sm text-gray-700"> Seleccione el rango </span>
 
-          <button
-            type="button"
-            className="text-sm text-gray-900 underline underline-offset-4"
-            onClick={() => {
-              setPriceFrom('');
-              setPriceTo('');
-              if (refFrom.current != null) {
-                refFrom.current.value = '';
-              }
-              if (refTo.current != null) {
-                refTo.current.value = '';
-              }
-            }}
-          >
-            Reset
-          </button>
-        </header>
+                    <button
+                        type="button"
+                        className="text-sm text-gray-900 underline underline-offset-4"
+                        onClick={() => {
+                            setPriceFrom('');
+                            setPriceTo('');
+                            if (refFrom.current != null) {
+                                refFrom.current.value = '';
+                            }
+                            if (refTo.current != null) {
+                                refTo.current.value = '';
+                            }
+                        }}
+                    >
+                        Reset
+                    </button>
+                </header>
 
-        <div className="border-t border-gray-200 p-4">
-          <div className="flex justify-between gap-4">
-            <label htmlFor="FilterPriceFrom" className="flex items-center gap-2">
-              <span className="text-sm text-gray-600">$</span>
+                <div className="border-t border-gray-200 p-4">
+                    <div className="flex justify-between gap-4">
+                        <label htmlFor="FilterPriceFrom" className="flex items-center gap-2">
+                            <span className="text-sm text-gray-600">$</span>
 
-              <input
-                type="number"
-                id="FilterPriceFrom"
-                placeholder="Desde"
-                className="w-full border-b border-gray-200 focus:outline-none focus:border-tertiary"
-                onChange={handleChangeFrom}
-                ref={refFrom}
-              />
-            </label>
+                            <input
+                                type="number"
+                                id="FilterPriceFrom"
+                                placeholder="Desde"
+                                className="w-full border-b border-gray-200 focus:outline-none focus:border-tertiary"
+                                onChange={handleChangeFrom}
+                                ref={refFrom}
+                            />
+                        </label>
 
-            <label htmlFor="FilterPriceTo" className="flex items-center gap-2">
-              <span className="text-sm text-gray-600">$</span>
+                        <label htmlFor="FilterPriceTo" className="flex items-center gap-2">
+                            <span className="text-sm text-gray-600">$</span>
 
-              <input
-                type="number"
-                id="FilterPriceTo"
-                placeholder="Hasta"
-                className="w-full border-b border-gray-200 focus:outline-none focus:border-tertiary"
-                onChange={handleChangeTo}
-                ref={refTo}
-              />
-            </label>
-          </div>
-        </div>
-      </div>
-    </details>
-  )
+                            <input
+                                type="number"
+                                id="FilterPriceTo"
+                                placeholder="Hasta"
+                                className="w-full border-b border-gray-200 focus:outline-none focus:border-tertiary"
+                                onChange={handleChangeTo}
+                                ref={refTo}
+                            />
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </details>
+    )
 }
